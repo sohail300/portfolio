200420ca807097b5ce67695651420c565215d5d8
sohail808 <sohailworks10@gmail.com>
2024-03-30 19:33:02 +0530

    pet projects added

--- src/utils/petProjects.ts
+++ src/utils/petProjects.ts
@@ -2,16 +2,18 @@ const petProjects = [
   {
     title: "Hangman Game",
     status: "Deployed",
-    description: "Hangman is a classic word guessing game where one player thinks of a word and the other player tries to guess it by suggesting letters within a certain number of guesses.",
+    description:
+      "Hangman is a classic word guessing game where one player thinks of a word and the other player tries to guess it by suggesting letters within a certain number of guesses.",
     imageUrl: "./petproject.png",
-    techStack: ["Next JS", "JavaScript"],
+    techStack: ["HTML", "CSS", "JavaScript"],
     gitLink: "https://github.com/sohail300/06-Hangman-Game",
     liveLink: "https://sohail300.github.io/06-Hangman-Game/",
   },
   {
     title: "Exchange Rate Calculator",
     status: "Deployed",
-    description: "An exchange rate calculator is a tool that allows users to convert the value of one currency into another currency.",
+    description:
+      "An exchange rate calculator is a tool that allows users to convert the value of one currency into another currency.",
     imageUrl: "./petproject.png",
     techStack: ["Next JS", "JavaScript"],
     gitLink: "https://github.com/sohail300/04-Exchange-Rate-Calculator",
@@ -20,40 +22,43 @@ const petProjects = [
   {
     title: "Movie Seat Booking",
     status: "Deployed",
-    description: "A movie seat booking application allows users to select seats for a movie in a theater and make reservations.",
+    description:
+      "A movie seat booking application allows users to select seats for a movie in a theater and make reservations.",
     imageUrl: "./petproject.png",
-    techStack: ["Next JS", "JavaScript"],
+    techStack: ["HTML", "CSS", "JavaScript"],
     gitLink: "https://github.com/sohail300/02-Movie-Seat-Booking",
     liveLink: "https://sohail300.github.io/02-Movie-Seat-Booking/",
   },
   {
     title: "Color Guesser Game",
     status: "Deployed",
-    description: "A color guesser game presents the player with a color and requires them to guess its RGB or hexadecimal value.",
+    description:
+      "A color guesser game presents the player with a color and requires them to guess its RGB or hexadecimal value.",
     imageUrl: "./petproject.png",
-    techStack: ["Next JS", "JavaScript"],
+    techStack: ["HTML", "CSS", "JavaScript"],
     gitLink: "https://github.com/sohail300/color-guesser-game",
     liveLink: "https://sohail300.github.io/Color-Guesser-Game/",
   },
   {
     title: "Dice Game",
     status: "Deployed",
-    description: "A dice game involves rolling one or more dice to achieve a certain outcome, often involving points or a race to reach a target score.",
+    description:
+      "A dice game involves rolling one or more dice to achieve a certain outcome, often involving points or a race to reach a target score.",
     imageUrl: "./petproject.png",
-    techStack: ["Next JS", "JavaScript"],
+    techStack: ["HTML", "CSS", "JavaScript"],
     gitLink: "https://github.com/sohail300/dice-game",
     liveLink: "https://sohail300.github.io/Dice-Game/",
   },
   {
     title: "Score Keeper",
     status: "Deployed",
-    description: "A score keeper application helps users keep track of scores for various games or activities.",
+    description:
+      "A score keeper application helps users keep track of scores for various games or activities.",
     imageUrl: "./petproject.png",
-    techStack: ["Next JS", "JavaScript"],
+    techStack: ["HTML", "CSS", "JavaScript"],
     gitLink: "https://github.com/sohail300/score-keeper",
     liveLink: "https://sohail300.github.io/Score-Keeper/",
   },
 ];
-
 
 export default petProjects;
