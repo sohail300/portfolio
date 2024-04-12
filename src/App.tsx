2cfecc91f2e879d8dc72228b6db73cd8aa4719cb
sohail808 <sohailworks10@gmail.com>
2024-04-13 03:03:02 +0530

    mobile version added

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import Experience from './components/Experience'
 import Hero from './components/Hero'
 import Navbar from './components/Navbar'
 // import Projects from './components/Projects'
-import Skills from './components/Skills'
+// import Skills from './components/Skills'
 
 function App() {
 
