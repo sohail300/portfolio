9956a904650d43cd33132d8a9b0f3c53459f64fa
sohail808 <sohailworks10@gmail.com>
2024-04-14 22:30:44 +0530

    Tab version updated

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -11,12 +11,11 @@ const Hero = () => {
         backgroundPosition: "center",
         backgroundSize: "cover",
       }}
-      className=" flex flex-col-reverse items-center justify-center py-20 md:flex-row lg:justify-evenly"
-      // md:py-40 
+      className=" flex flex-col-reverse items-center justify-center py-20 md:py-40 lg:flex-row lg:justify-evenly "
       id="home"
     >
       <motion.div
-        className=" flex flex-col items-center justify-center md:w-2/5 md:items-start"
+        className=" flex flex-col items-center justify-center lg:w-2/5 lg:items-start"
         initial={{
           opacity: 0,
           y: 100,
@@ -31,7 +30,7 @@ const Hero = () => {
           },
         }}
       >
-        <div className=" text-white px-4 w-screen md:w-auto md:px-0">
+        <div className=" text-white px-4 w-screen text-center lg:w-auto lg:px-0 lg:mx-0 lg:text-left">
           <TypeAnimation
             sequence={[
               "Hey, I'm Sohail",
@@ -55,16 +54,16 @@ const Hero = () => {
             repeat={Infinity}
             className="gradient-text text-animation"
           />
-          <div className=" flex flex-row items-center w-72 mb-8">
+          <div className=" flex flex-row items-center justify-center lg:justify-start lg:w-72 lg:mb-8">
             <PlaceIcon className=" mr-2" />
             Dhanbad, Jharkhand, India
             <ReactCountryFlag countryCode="IN" svg className=" ml-4" />
           </div>
         </div>
 
-        <div className=" md:self-start">
+        <div className=" mt-8 lg:mt-0 lg:self-start">
           <a
-            className=" py-2 px-28 bg-white rounded-md text-center font-semibold w-full md:px-20 "
+            className=" py-2 px-28 bg-white rounded-md text-center font-semibold w-full lg:px-20 "
             href="https://drive.google.com/file/d/1bVgqtiOsF9rqzj4ED7X_s1AhNL03_4dj/view?usp=drive_link"
             target="_blank"
             style={{ color: "#004632" }}
@@ -90,7 +89,7 @@ const Hero = () => {
         <img
           src="https://res.cloudinary.com/dwuzfbivo/image/upload/f_auto/v1711872116/portfolio/hero_nd6kag.png"
           alt=""
-          className=" w-80 lg:w-auto mb-16 md:mb-0"
+          className=" w-80 md:w-auto lg:w-auto mb-16 md:mb-32 lg:mb-0"
         />
       </motion.div>
     </div>
