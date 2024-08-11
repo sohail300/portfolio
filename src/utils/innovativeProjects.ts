90a2b8174b41078384d9f7fbd37020ed55fa9260
sohail808 <sohailworks10@gmail.com>
2024-08-11 18:54:07 +0530

    projects added

--- src/utils/innovativeProjects.ts
+++ src/utils/innovativeProjects.ts
@@ -52,26 +52,26 @@ const innovativeProjects = [
     liveLink: "https://invoice-scan.heysohail.me/",
     // youtubeLink: "",
   },
-  {
-    title: "Hotstar Ad Muter",
-    status: "Deployed",
-    description:
-      "An intelligent PDF processing application that automatically extracts critical information from invoice documents and displays them in a tabular format. Process images in the same way.",
-    imageUrl:
-      "https://res.cloudinary.com/dwuzfbivo/image/upload/v1723379028/portfolio/innovative%20projects/invoicescan_pn9nry.png",
-    techStack: [
-      "React",
-      "ShadCN",
-      "TypeScript",
-      "ExpressJS",
-      "Multer",
-      "Gemini AI",
-      "Zod",
-    ],
-    gitLink: "https://github.com/sohail300/Hotstar-Ad-Muter",
-    liveLink: "https://invoice-scan.heysohail.me/",
-    // youtubeLink: "",
-  },
+  //   {
+  //     title: "Hotstar Ad Muter",
+  //     status: "Deployed",
+  //     description:
+  //       "An intelligent PDF processing application that automatically extracts critical information from invoice documents and displays them in a tabular format. Process images in the same way.",
+  //     imageUrl:
+  //       "https://res.cloudinary.com/dwuzfbivo/image/upload/v1723379028/portfolio/innovative%20projects/invoicescan_pn9nry.png",
+  //     techStack: [
+  //       "React",
+  //       "ShadCN",
+  //       "TypeScript",
+  //       "ExpressJS",
+  //       "Multer",
+  //       "Gemini AI",
+  //       "Zod",
+  //     ],
+  //     gitLink: "https://github.com/sohail300/Hotstar-Ad-Muter",
+  //     liveLink: "https://invoice-scan.heysohail.me/",
+  //     // youtubeLink: "",
+  //   },
 ];
 
 export default innovativeProjects;
